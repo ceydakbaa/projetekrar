1a946efed49c603bb6f7fa5866d38af5986392fb
Ceyda Kaba <ceydakaba@gmail.com>
2025-06-07 16:30:57 +0300

    c

---
+++ src/main/java/com/ceyda/Main.java
@@ -0,0 +1,28 @@
+package com.ceyda;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       // Java Veri Tipleri Projesi
+        int i = 10;
+        System.out.println("int i :"+ 10);
+
+        double number= 9.8;
+        System.out.println("double number :"+ 9.8);
+
+        float a = 3.17f;
+        System.out.println("float number:"+ 3.17f);
+
+        boolean trueorfalse= true;
+        System.out.println("booleon :"+ true);
+        String name = "Ceyda";
+        System.out.println("String :"+ name);
+        final int sayı;
+        sayı=4;
+        System.out.println("final int sayı :"+ sayı);
+
+
+
+    }
+}
